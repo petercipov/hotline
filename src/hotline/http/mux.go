afacb296adeb0be794865c42311c096e5affa521
Peter Cipov <petercipov@gmail.com>
2025-06-20 15:38:22 +0200

    adding route pattern for custom mux

---
+++ src/hotline/http/mux.go
@@ -0,0 +1,101 @@
+package http
+
+import (
+	"strings"
+)
+
+type RoutePattern struct {
+	Method      string
+	PathPattern string // /user/{user-id}/login/{session-id}/
+
+	preParsed   []pathPart
+	methodLower string
+}
+
+func NewRoutePattern(method string, pathPattern string) *RoutePattern {
+	return &RoutePattern{
+		Method:      method,
+		PathPattern: pathPattern,
+
+		methodLower: strings.ToLower(method),
+		preParsed:   parsePath(pathPattern),
+	}
+}
+
+type pathPart struct {
+	valueLower string
+	isWildcard bool
+	name       string // name of the wildcard (e.g., "user-id" for {user-id})
+}
+
+const urlDelimiter = "/"
+
+func parsePath(pathPattern string) []pathPart {
+	parts := normalizedPathParts(pathPattern)
+	if len(parts) == 0 {
+		return []pathPart{}
+	}
+
+	pathParts := make([]pathPart, 0, len(parts))
+	for _, part := range parts {
+		isWildcard, name := parseWildcard(part)
+		pathParts = append(pathParts, pathPart{
+			valueLower: part,
+			isWildcard: isWildcard,
+			name:       name,
+		})
+	}
+
+	return pathParts
+}
+
+func parseWildcard(part string) (bool, string) {
+	if len(part) >= 2 && part[0] == '{' && part[len(part)-1] == '}' {
+		name := part[1 : len(part)-1]
+		return true, name
+	}
+	return false, ""
+}
+
+func (p *RoutePattern) Matches(method, path string) bool {
+	if p.methodLower != strings.ToLower(method) {
+		return false
+	}
+
+	return p.matchesPath(path)
+}
+
+func (p *RoutePattern) matchesPath(path string) bool {
+	if len(p.preParsed) == 0 {
+		return path == "/"
+	}
+
+	pathParts := normalizedPathParts(path)
+
+	if len(pathParts) < len(p.preParsed) {
+		return false
+	}
+
+	for i, part := range p.preParsed {
+		if part.isWildcard {
+			continue
+		}
+
+		if part.valueLower != pathParts[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func normalizedPathParts(path string) []string {
+	var pathParts []string
+	parts := strings.Split(strings.Trim(path, urlDelimiter), urlDelimiter)
+	pathParts = make([]string, 0, len(parts))
+	for _, part := range parts {
+		if len(part) != 0 {
+			pathParts = append(pathParts, strings.ToLower(part))
+		}
+	}
+	return pathParts
+}
---
+++ src/hotline/http/mux_test.go
@@ -0,0 +1,101 @@
+package http_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"hotline/http"
+)
+
+var _ = Describe("HTTP RoutePattern Matching", func() {
+
+	Context("Just Root Path", func() {
+		pattern := http.NewRoutePattern("GET", "/")
+
+		DescribeTable("Mismatches All except root",
+			func(method, path string) {
+				match := pattern.Matches(method, path)
+				Expect(match).To(BeFalse())
+			},
+			Entry("mismatch empty", "GET", ""),
+			Entry("mismatch root", "GET", "/users"),
+		)
+
+		It("matches root", func() {
+			match := pattern.Matches("GET", "/")
+			Expect(match).To(BeTrue())
+		})
+	})
+
+	Context("Simple path", func() {
+		pattern := http.NewRoutePattern("Get", "/users")
+
+		DescribeTable("Mismatches",
+			func(method, path string) {
+				match := pattern.Matches(method, path)
+				Expect(match).To(BeFalse())
+			},
+			Entry("mismatch in http method", "POST", "/users"),
+			Entry("mismatch in path", "GET", "/posts"),
+			Entry("mismatch in root", "GET", "/"),
+		)
+
+		DescribeTable("Matches",
+			func(method, path string) {
+				match := pattern.Matches(method, path)
+				Expect(match).To(BeTrue())
+			},
+			Entry("match", "GET", "/users"),
+			Entry("match in sub path", "GET", "/users/1234567890"),
+			Entry("match in sub sub path", "GET", "/users/1234567890/logins"),
+			Entry("match in anycase path", "GET", "/UsErs"),
+			Entry("match in anycase host", "GET", "/users"),
+		)
+	})
+
+	Context("Named Wildcard", func() {
+		pattern := http.NewRoutePattern("GET", "/users/{user-id}")
+
+		DescribeTable("Mismatches",
+			func(method, path string) {
+				match := pattern.Matches(method, path)
+				Expect(match).To(BeFalse())
+			},
+			Entry("mismatch in short path", "GET", "/users"),
+			Entry("mismatch in root", "GET", "/"),
+		)
+
+		DescribeTable("Matches",
+			func(method, path string) {
+				match := pattern.Matches(method, path)
+				Expect(match).To(BeTrue())
+			},
+			Entry("match in path", "GET", "/users/1234567890"),
+			Entry("match in longer path", "GET", "/users/1234567890/logins"),
+		)
+	})
+
+	Context("Multi Named Wildcard", func() {
+		pattern := http.NewRoutePattern("GET", "/users/{user-id}/logins/{login-id}")
+
+		DescribeTable("Mismatches",
+			func(method, path string) {
+				match := pattern.Matches(method, path)
+				Expect(match).To(BeFalse())
+			},
+			Entry("mismatch in 1. level", "GET", "/users"),
+			Entry("mismatch in 2. level", "GET", "/users/12345"),
+			Entry("mismatch in 3. level", "GET", "/users/12345/logins"),
+			Entry("mismatch in 3. level slash", "GET", "/users/12345/logins/"),
+		)
+
+		DescribeTable("Matches",
+			func(method, path string) {
+				match := pattern.Matches(method, path)
+				Expect(match).To(BeTrue())
+			},
+			Entry("match multi wildcard", "GET", "/users/1234567890/logins/L1234567890"),
+			Entry("match multi wildcard slash", "GET", "/users/1234567890/logins/L1234567890/"),
+			Entry("match multi wildcard sub", "GET", "/users/1234567890/logins/L1234567890/sub"),
+		)
+	})
+})
